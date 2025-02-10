7a4a4064ee8cfdf6da31da7a9ec191dd68ebf6a0
leondbtune <leondbt@protonmai.com>
2025-02-10 22:07:40 +0100

    added primevue component library

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-
+import Button from "primevue/button"
 defineProps({
   msg: String,
 })
@@ -18,6 +18,8 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
+
+  <Button>Verify</Button>
 
   <p>
     Check out
