a98aaac1dc3e82d99f385a7ee05392602a32d527
leondbt <leondbt@protonmail.com>
2025-02-25 13:22:47 +0100

    nuxthub  caching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxthub/core'],
   css: ['@fortawesome/fontawesome-free/css/all.min.css'],
   runtimeConfig: {
     public: {
@@ -10,4 +10,7 @@ export default defineNuxtConfig({
     }
 
   },
+  'hub': {
+    'cache': true
+  }
 })
